8d0e5c9f2dd521a6161ee6e59599de244e533fbd
Rohit Sharma <rohithsharma84@gmail.com>
2025-02-15 19:06:42 -0500

    File created

---
+++ test_lists.py
@@ -0,0 +1,17 @@
+# Testing how to use lists
+
+my_list = ["A","C","D","B","F","E"]
+
+print("List as entered...")
+print(my_list)
+
+print("sorted list...")
+my_list.sort()
+print(my_list)
+
+print("Reverse sort...")
+my_list.reverse()
+print(my_list)
+
+print("Number of elements...")
+print(len(my_list))
